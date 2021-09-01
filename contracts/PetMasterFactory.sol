6eaf6b802c812814b92ab3117f9f17957ea72d32
Lew Tran <lewtran@outlook.com>
2021-09-01 16:01:36 +0700

    Staking contract V1 implementation

---
+++ contracts/PetMasterFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.6.6;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './PetMaster.sol';
+
+contract PetMasterFactory is Ownable {
+    event PetMasterCreated(address indexed petMaster);
+
+    constructor() public {}
+
+    function createPetMaster(
+        address _token,
+        uint256 _startBlock,
+        uint256 _tokenPerBlock,
+        uint256 _maxTokenPerBlock,
+        uint256 _totalToBeMintAmount
+    ) external onlyOwner returns (address) {
+        PetMaster petMaster = new PetMaster(
+            _token,
+            _startBlock,
+            _tokenPerBlock,
+            _maxTokenPerBlock,
+            _totalToBeMintAmount
+        );
+        Ownable(address(petMaster)).transferOwnership(_msgSender());
+        emit PetMasterCreated(address(petMaster));
+        return address(petMaster);
+    }
+}
